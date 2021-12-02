2cbd1f6b87417941339df51c7c6d2aa42f41eafb
LoganCoats22 <logancoats222@gmail.com>
2021-12-02 15:21:15 -0600

    FInished prototyping struct for api

--- Project_1/ViewController.swift
+++ Project_1/ViewController.swift
@@ -107,6 +107,68 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                 let icon: String
             }
         }
+        struct Minutely: Codable{
+            let dt: Int
+            let precipitation: Int
+        }
+        struct Hourly: Codable{
+            let dt: Int
+            let sunrise: Int
+            let sunset: Int
+            let temp: Double
+            let feels_like: Double
+            let pressure: Int
+            let humidity: Int
+            let dew_point: Double
+            let uvi: Double
+            let clouds: Int
+            let visibility: Int
+            let wind_speed: Int
+            let wind_deg: Int
+            struct Weather: Codable{
+                let id: Int
+                let main: String
+                let description: String
+                let icon: String
+            }
+        }
+        struct Daily: Codable{
+            let dt: Int
+            let sunrise: Int
+            let sunset: Int
+            let moonrise: Int
+            let moonset: Int
+            let moon_phase: Double
+            struct temp: Codable{
+                let day: Double
+                let min: Double
+                let max: Double
+                let night: Double
+                let eve: Double
+                let morn: Double
+            }
+            struct feels_like: Codable{
+                let day: Double
+                let night: Double
+                let eve: Double
+                let morn: Double
+            }
+            let pressure: Int
+            let humidity: Int
+            let dew_point: Double
+            let wind_speed: Double
+            let wind_deg: Int
+            let wind_gust: Double
+            struct weather{
+                let id: Int
+                let main: String
+                let description: String
+                let icon: String
+            }
+            let clouds: Int
+            let pop: Int
+            let uvi: Double
+        }
     }
 }
             
