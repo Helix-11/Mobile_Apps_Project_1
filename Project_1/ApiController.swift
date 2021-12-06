91f4b4b588f1bb65479cdffee645559f9e18ece1
Jonathan Velkeneers <jonathanvelkeneers@hotmail.com>
2021-12-05 18:12:36 -0600

    Add Apicontroller file, clean up code and fix callback function

---
+++ Project_1/ApiController.swift
@@ -0,0 +1,92 @@
+//
+//  ApiController.swift
+//  Project_1
+//
+//  Created by student on 12/5/21.
+//
+
+import Foundation
+import UIKit
+
+class ApiController{
+    private let url = "https://api.openweathermap.org/data/2.5/onecall?lat=40.354386155103285&lon=-94.88243178493983&units=imperial&appid=e4bbcb36109771706e78bc6514dd98e3"
+    
+    func getData(completion: @escaping (Forecast)->()){
+            let task = URLSession.shared.dataTask(with: URL(string:url)!, completionHandler: { data, response, error in
+                
+                guard let data = data, error == nil else {
+                    print("something went wrong")
+                    return
+                }
+                var result: Forecast?
+                do{
+                    result = try JSONDecoder().decode(Forecast.self, from: data)
+                }
+                catch{
+                    print("failed to convert: \(error)")
+                }
+                guard result != nil else{
+                    return
+                }
+                // print(result!)
+                completion(result!)
+            })
+            task.resume()
+        }
+}
+
+struct Forecast: Codable {
+    let lat: Double
+    let lon: Double
+    let timezone: String
+    let timezone_offset: Int
+    struct Current: Codable{
+        let dt: Int
+        let sunrise: Int
+        let sunset: Int
+        let temp: Double
+        let feels_like: Double
+        let pressure: Int
+        let humidity: Int
+        let dew_point: Double
+        let uvi: Double
+        let clouds: Int
+        let visibility: Int
+        let wind_speed: Double
+        let wind_deg: Int
+        struct Weather: Codable{
+            let id: Int
+            let main: String
+            let description: String
+            let icon: String
+        }
+        let weather: [Weather]
+    }
+    let current: Current
+    struct Daily: Codable {
+        let dt: Date
+        struct Temp: Codable {
+            let min: Double
+            let max: Double
+        }
+        let temp: Temp
+        let humidity: Int
+        struct Weather: Codable {
+            let id: Int
+            let description: String
+            let icon: String
+            var weatherIconURL: URL {
+                let urlString = "http://openweathermap.org/img/wn/\(icon)@2x.png"
+                return URL(string: urlString)!
+            }
+        }
+        let weather: [Weather]
+        let clouds: Int
+        let pop: Double
+    }
+    let daily: [Daily]
+}
+
+enum APIError: Error {
+    case error( errorString: String)
+}
--- Project_1/ViewController.swift
+++ Project_1/ViewController.swift
@@ -34,9 +34,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     @IBOutlet weak var forecastTableView: UITableView!
     
     var weatherArray = [WeatherDay]()
-    let url = "https://api.openweathermap.org/data/2.5/onecall?lat=40.354386155103285&lon=-94.88243178493983&units=imperial&appid=e4bbcb36109771706e78bc6514dd98e3"
-    
     var results: Forecast?
+    let apiController = ApiController()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         forecastTableView.delegate = self
@@ -46,20 +47,11 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let test2 = WeatherDay(temperature: 85, detail: "dry", minTemp: 5, maxTemp: 99, imageName: "image2", date: Date.now)
         weatherArray.append(test1)
         weatherArray.append(test2)
-        getData2(from: url)
-        getData(){ (result: Result<Forecast,APIError>) in
-            switch result{
-            case .success(let forecast):
-                for day in forecast.daily{
-                    print (day)
-                }
-            case .failure(let apiError):
-                switch apiError{
-                case .error(let errorString):
-                    print(errorString)
-                }
-            }
+        
+        apiController.getData { (result)->() in
+            print(result)
         }
+        
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -81,120 +73,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
     }
     
-    enum APIError: Error {
-        case error( errorString: String)
-    }
-    
-    private func getData2(from url: String){
-            let task = URLSession.shared.dataTask(with: URL(string:url)!, completionHandler: {data, response, error in
-                
-                guard let data = data, error == nil else {
-                    print("something went wrong")
-                    return
-                }
-                var result: Forecast?
-                do{
-                    result = try JSONDecoder().decode(Forecast.self, from: data)
-                }
-                catch{
-                    print("failed to convert: \(error)")
-                }
-                guard let json = result else{
-                    return
-                }
-                print(result!)
-                self.results = result
-            })
-            task.resume()
-        }
-
-    func getData<T: Decodable>(completion: @escaping (Result<T,APIError>) -> Void){
-        guard let url = URL(string: url)else{
-            completion(.failure(.error(errorString: NSLocalizedString("Error: Invalid URL", comment: ""))))
-            print("invalid url")
-            return
-        }
-        let request = URLRequest(url: url)
-        URLSession.shared.dataTask(with:request) { (data, response, error) in
-            if let error = error {
-                completion(.failure(.error(errorString: "Error: \(error.localizedDescription)")))
-                print("error: \(error.localizedDescription)")
-                return
-            }
-            
-            guard let data = data else{
-                completion(.failure(.error(errorString: NSLocalizedString("Error: Data is corrupt.", comment: ""))))
-                print("data is corrupt")
-                return
-            }
-            let decoder = JSONDecoder()
-            decoder.dateDecodingStrategy = .secondsSince1970
-            decoder.keyDecodingStrategy = .useDefaultKeys
-            do{
-                let decodedData = try decoder.decode(T.self, from: data)
-                completion(.success(decodedData))
-                print("data recieved: \(decodedData)")
-                return
-            }catch let decodingError {
-                completion(.failure(APIError.error(errorString: "Error: \(decodingError.localizedDescription)")))
-                print("error: \(decodingError.localizedDescription)")
-            }
-            
-        }
-        
-    }
-        
-    struct Forecast: Codable {
-        let lat: Double
-        let lon: Double
-        let timezone: String
-        let timezone_offset: Int
-        struct Current: Codable{
-            let dt: Int
-            let sunrise: Int
-            let sunset: Int
-            let temp: Double
-            let feels_like: Double
-            let pressure: Int
-            let humidity: Int
-            let dew_point: Double
-            let uvi: Double
-            let clouds: Int
-            let visibility: Int
-            let wind_speed: Double
-            let wind_deg: Int
-            struct Weather: Codable{
-                let id: Int
-                let main: String
-                let description: String
-                let icon: String
-            }
-            let weather: [Weather]
-        }
-        let current: Current
-        struct Daily: Codable {
-            let dt: Date
-            struct Temp: Codable {
-                let min: Double
-                let max: Double
-            }
-            let temp: Temp
-            let humidity: Int
-            struct Weather: Codable {
-                let id: Int
-                let description: String
-                let icon: String
-                var weatherIconURL: URL {
-                    let urlString = "http://openweathermap.org/img/wn/\(icon)@2x.png"
-                    return URL(string: urlString)!
-                }
-            }
-            let weather: [Weather]
-            let clouds: Int
-            let pop: Double
-        }
-        let daily: [Daily]
-    }
     
     func loadImage(url: URL) {
         DispatchQueue.global().async {
